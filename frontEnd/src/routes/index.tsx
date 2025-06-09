c38c851eb29ca864f1d8dccad07d1d1b065f613c
413ksz <developer413ksz@gmail.com>
2025-06-09 12:41:45 +0200

    chore: :mag: Added meta tags to homepage
    Added meta tags to homepage for better seo

--- frontEnd/src/routes/index.tsx
+++ frontEnd/src/routes/index.tsx
@@ -1,3 +1,4 @@
+import { Link, Meta, Title } from "@solidjs/meta";
 import Developer from "~/components/homePage/Developer";
 import Features from "~/components/homePage/Features";
 import Footer from "~/components/homePage/Footer";
@@ -8,6 +9,16 @@ import Technologies from "~/components/homePage/Technologies";
 export default function Home() {
   return (
     <div class="min-h-screen bg-gradient-to-br from-gray-900 via-blue-900 to-blue-950 text-white flex flex-col">
+      <Title>Blue Fox - Modern Open-Source Real-time Chat Application</Title>
+      <Meta
+        name="description"
+        content="Blue Fox is a blazing fast, open-source real-time chat application built with SolidJS and Tailwind CSS. Connect instantly and join a vibrant community."
+      />
+      <Meta
+        name="keywords"
+        content="Blue Fox, chat app, real-time chat, open source, SolidJS, Tailwind CSS, community, communication, web app, real-time messaging platform, SolidJS web application example"
+      />
+      <Link rel="canonical" href="https://blue-fox.vercel.app/" />
       <Navbar />
       <Hero />
       <Features />
