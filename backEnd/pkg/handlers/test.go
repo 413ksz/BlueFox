a9bf0b7397fe792e4a1dc83a75618ce34c2c0c1e
413ksz <developer413ksz@gmail.com>
2025-06-12 15:01:47 +0200

    chore: :recycle: Refactored test.go to be able to test like in a real env
    Not logging the error gives back json

--- backEnd/pkg/handlers/test.go
+++ backEnd/pkg/handlers/test.go
@@ -1,13 +1,31 @@
 package handlers
 
 import (
-	"fmt"
-	"log"
+	"encoding/json"
 	"net/http"
 )
 
+type TestResponse struct {
+	Message string `json:"message"`
+	Status  string `json:"status"`
+}
+
 func TestHandler(w http.ResponseWriter, r *http.Request) {
-	log.Println("Executing test handler.")
+	// Create an instance of our response struct
+	response := TestResponse{
+		Message: "Hello from the /api/test endpoint! Routing works!",
+		Status:  "success",
+	}
+
+	// Set the Content-Type header to application/json
+	w.Header().Set("Content-Type", "application/json")
+
+	// Set the HTTP status code
 	w.WriteHeader(http.StatusOK)
-	fmt.Fprint(w, "Hello from the /api/test endpoint! Routing works!\n")
+
+	// Encode the response struct to JSON and write it to the response writer
+	err := json.NewEncoder(w).Encode(response)
+	if err != nil {
+		http.Error(w, "Internal server error", http.StatusInternalServerError)
+	}
 }
