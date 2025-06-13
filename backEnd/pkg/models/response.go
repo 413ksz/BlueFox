fc472c94cf1cab4559cf900c7618fd01ded02d46
413ksz <developer413ksz@gmail.com>
2025-06-13 09:27:33 +0200

    chore: :label: Add api response type
    Add api response type

---
+++ backEnd/pkg/models/response.go
@@ -0,0 +1,35 @@
+package models
+
+import (
+	"time"
+)
+
+type CustomError struct {
+	Domain  string `json:"domain"`
+	Reason  string `json:"reason"`
+	Message string `json:"message"`
+}
+
+type Pagination struct {
+	TotalItems   int     `json:"totalItems"`
+	ItemsPerPage *int    `json:"itemsPerPage,omitempty"`
+	PageIndex    *int    `json:"pageIndex,omitempty"`
+	NextLink     *string `json:"nextLink,omitempty"`
+	PreviousLink *string `json:"previousLink,omitempty"`
+}
+
+type ResponseData[TItemtype any, TFieldtype any] struct {
+	Fields     *TFieldtype `json:"fields,omitempty"`
+	Deleted    *bool       `json:"deleted,omitempty"`
+	Updated    *time.Time  `json:"updated,omitempty"`
+	Pagination *Pagination `json:"pagination,omitempty"`
+	Items      []TItemtype `json:"items"`
+}
+
+type ApiResponse[TItemtype any, TFieldtype any] struct {
+	Context *string                              `json:"context,omitempty"`
+	Method  *string                              `json:"method,omitempty"`
+	Params  map[string]interface{}               `json:"params,omitempty"`
+	Data    *ResponseData[TItemtype, TFieldtype] `json:"data,omitempty"`
+	Error   *CustomError                         `json:"error,omitempty"`
+}
