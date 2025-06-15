f6328b40d8c7b11681df362e49b71f35ed338b1e
413ksz <developer413ksz@gmail.com>
2025-06-15 22:45:24 +0200

    feat: :sparkles: Updated user login
    Updated user login

--- backEnd/pkg/handlers/user_login.go
+++ backEnd/pkg/handlers/user_login.go
@@ -8,6 +8,7 @@ import (
 	"github.com/413ksz/BlueFox/backEnd/pkg/apierrors"
 	"github.com/413ksz/BlueFox/backEnd/pkg/database"
 	"github.com/413ksz/BlueFox/backEnd/pkg/models"
+	passwordHashing "github.com/413ksz/BlueFox/backEnd/pkg/password_hashing"
 	jwt_token "github.com/413ksz/BlueFox/backEnd/pkg/token"
 	"github.com/413ksz/BlueFox/backEnd/pkg/validation"
 	"gorm.io/gorm"
@@ -67,8 +68,9 @@ func UserLoginHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	fetchedUser := models.User{}
 	// Attempt to find a user with the provided email and password in the database.
-	result := db.Where("email = ? AND password = ?", user.Email, user.Password).Preload("ProfilePictureAsset").First(&user).Select("id", "username", "profile_picture_asset_id")
+	result := db.Where("email = ?", user.Email).Preload("ProfilePictureAsset").First(&fetchedUser).Select("id", "username", "profile_picture_asset_id")
 	if result.Error != nil {
 		if result.Error == gorm.ErrRecordNotFound {
 			apiResponse.Error = apierrors.ERROR_CODE_NOT_FOUND.ApiErrorResponse("User not found", nil)
@@ -82,16 +84,22 @@ func UserLoginHandler(w http.ResponseWriter, r *http.Request) {
 		models.SendApiResponse(w, apiResponse)
 		return
 	}
+	if !passwordHashing.VerifyPassword(user.Password, fetchedUser.Password) {
+		apiResponse.Error = apierrors.ERROR_CODE_UNAUTHORIZED.ApiErrorResponse("Invalid credentials", nil)
+		log.Printf("ERROR: [%s][%s] Invalid credentials for user ID %s", apiResponse.Context, apiResponse.Method, user.Email)
+		models.SendApiResponse(w, apiResponse)
+		return
+	}
 
 	// Generate a JWT token for the user
-	token, err := jwt_token.GenerateJWTToken(user.Username, user.ID.String(), user.ProfilePictureAssetID.String())
+	token, err := jwt_token.GenerateJWTToken(user.Username, user.ID.String(), user.ProfilePictureAsset.UrlPath)
 
 	if err != nil {
 		apiResponse.Error = apierrors.ERROR_CODE_DATABASE_ERROR.ApiErrorResponse("Error generating JWT token", nil)
 		log.Printf("ERROR: [%s][%s] Error generating JWT token for user ID %s: %v", apiResponse.Context, apiResponse.Method, user.Email, result.Error)
 		models.SendApiResponse(w, apiResponse)
 	}
-
+	w.Header().Set("Authorization", "Bearer: "+token)
 	models.SendApiResponse(w, apiResponse)
 
 }
