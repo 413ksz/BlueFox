e3017802537b38f9fde26e90060fba40bec03f4d
413ksz <developer413ksz@gmail.com>
2025-06-18 12:09:56 +0200

    chore: :loud_sound: Added structured logs for Register routes func
    Added structured logs for Register routes func

--- backEnd/pkg/router/routes.go
+++ backEnd/pkg/router/routes.go
@@ -1,22 +1,29 @@
 package router
 
 import (
-	"log"
-
 	"github.com/413ksz/BlueFox/backEnd/pkg/handlers"
 	"github.com/413ksz/BlueFox/backEnd/pkg/handlers/user"
 	"github.com/gorilla/mux"
+	"github.com/rs/zerolog/log"
 )
 
 // RegisterRoutes adds routes from the handlers package to the given gorilla/mux router.
 func RegisterRoutes(r *mux.Router) {
-	r.HandleFunc("/api/test", handlers.TestHandler).Methods("GET")
+	log.Info().
+		Str("component", "router").
+		Str("event", "routes_register_start").
+		Msg("Started registering routes...")
 
+	r.HandleFunc("/api/test", handlers.TestHandler).Methods("GET")
 	r.HandleFunc("/api/user/{id}", user.UserGetHandler).Methods("GET")
 	r.HandleFunc("/api/user", user.UserCreateHandler).Methods("PUT")
 	r.HandleFunc("/api/user/{id}", handlers.TestHandler).Methods("DELETE")
 	r.HandleFunc("/api/user/login", user.UserLoginHandler).Methods("POST")
 	r.HandleFunc("/api/user/{id}", user.UserUpdateHandler).Methods("PATCH")
 
-	log.Println("Handlers package registered routes.")
+	log.Info().
+		Str("component", "router").
+		Str("event", "routes_register_finished").
+		Msg("Finished registering routes...")
+
 }
